d795856fd9cdcdf0b0bc9630c39051baceebc56a
jgarnicaa <jgarnicaa@unal.edu.co>
2024-12-20 21:32:58 +0100

    First webapp version. Croquis

--- webapp/app.py
+++ webapp/app.py
@@ -5,7 +5,7 @@ from waitress import serve
 app=Flask(__name__)
 
 @app.route('/')
-@app.route('/index', methods=['POST'])
+@app.route('/index')
 
 def index():
     return render_template('index.html')
@@ -14,28 +14,28 @@ def index():
 
 def get_status():
     data = [
-        int(request.args.get('code_gender', 0)),
-        int(request.args.get('flag_own_car', 0)),
-        int(request.args.get('flag_own_realty', 0)),
-        int(request.args.get('cnt_children', 0)),
-        int(request.args.get('AMT_INCOME_TOTAL', 0)),
-        int(request.args.get('name_income_type', 0)),
-        int(request.args.get('name_education_type', 0)),
-        int(request.args.get('name_family_status', 0)),
-        int(request.args.get('name_housing_type', 0)),
-        int(request.args.get('days_birth', 0)),
-        int(request.args.get('days_employed', 0)),
-        int(request.args.get('flag_mobil', 0)),
-        int(request.args.get('flag_work_phone', 0)),
-        int(request.args.get('flag_phone', 0)),
-        int(request.args.get('flag_email', 0)),
-        int(request.args.get('occupation_type', 0)),
-        int(request.args.get('cnt_fam_members', 0))
+        int(request.form.get('code_gender', 0)),
+        int(request.form.get('flag_own_car', 0)),
+        int(request.form.get('flag_own_realty', 0)),
+        int(request.form.get('cnt_children', 0)),
+        int(request.form.get('AMT_INCOME_TOTAL', 0)),
+        int(request.form.get('name_income_type', 0)),
+        int(request.form.get('name_education_type', 0)),
+        int(request.form.get('name_family_status', 0)),
+        int(request.form.get('name_housing_type', 0)),
+        int(request.form.get('days_birth', 0)),
+        int(request.form.get('days_employed', 0)),
+        int(request.form.get('flag_mobil', 0)),
+        int(request.form.get('flag_work_phone', 0)),
+        int(request.form.get('flag_phone', 0)),
+        int(request.form.get('flag_email', 0)),
+        int(request.form.get('occupation_type', 0)),
+        int(request.form.get('cnt_fam_members', 0))
     ]
 
     status_data=(get_approval(data))
 
-    return render_template('result.html', status="status_data")
+    return render_template('result.html', status=status_data)
 
 
 if __name__ == "__main__":
